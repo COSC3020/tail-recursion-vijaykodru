b22f89d7e86b2d7fce557f72015093f0e31a051c
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-05-08 02:14:14 +0000

    test

--- code.test.js
+++ code.test.js
@@ -2,13 +2,6 @@ const fs = require("fs");
 const assert = require("assert");
 eval(fs.readFileSync('code.js').toString());
 
-assert(tailrecursiveFIB(1) === 0)
-assert(tailrecursiveFIB(2) === 1)
-assert(tailrecursiveFIB(3) === 1)
-assert(tailrecursiveFIB(4) === 2)
-assert(tailrecursiveFIB(5) === 3)
-assert(tailrecursiveFIB(7) === 8)
-assert(tailrecursiveFIB(9) === 21)
-assert(tailrecursiveFIB(10) === 34)
-assert(tailrecursiveFIB(30) === 832040)
-
+equate1 = tailrecursiveFIB(1);
+result1 = 0;
+assert(equate1 == result1)
