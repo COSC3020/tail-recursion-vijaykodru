05a56d60ad9377d259618ec2b961944d79be9490
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-05-08 02:26:42 +0000

    test

--- code.test.js
+++ code.test.js
@@ -7,11 +7,11 @@ result1 = 1;
 assert(equate1 == result1);
 
 
-assert(tailrecursiveFIB(3) === 2);
-assert(tailrecursiveFIB(4) === 3);
-assert(tailrecursiveFIB(5) === 5);
-assert(tailrecursiveFIB(7) === 13);
-assert(tailrecursiveFIB(9) === 34);
-assert(tailrecursiveFIB(10) === 55);
-assert(tailrecursiveFIB(15) === 987);
-assert(tailrecursiveFIB(25) === 121393);
+assert(tailrecursiveFIB(3) == 2);
+assert(tailrecursiveFIB(4) == 3);
+assert(tailrecursiveFIB(5) == 5);
+assert(tailrecursiveFIB(7) == 13);
+assert(tailrecursiveFIB(9) == 34);
+assert(tailrecursiveFIB(10) == 55);
+assert(tailrecursiveFIB(15) == 987);
+assert(tailrecursiveFIB(25) == 121393);
